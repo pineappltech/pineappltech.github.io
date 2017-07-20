96a9e9edd6348e7b47781dc6d5b752898fd9074f
pineappltech <jcoderapps@gmail.com>
2017-07-20 13:21:53 +0800

    Update and rename sw2.js to sw3.js

--- sw3.js
+++ sw3.js
@@ -3,7 +3,7 @@
 // Names of the two caches used in this version of the service worker.
 // Change to v2, etc. when you update any of the local resources, which will
 // in turn trigger the install event again.
-const PRECACHE = 'precache-v5';
+const PRECACHE = 'precache-v6';
 const RUNTIME = 'runtime';
 
 // A list of local resources we always want to be cached.
@@ -34,9 +34,9 @@ self.addEventListener('install', event => {
       .then(self.skipWaiting())
   );
   
-  	event.registerForeignFetch({
+    	event.registerForeignFetch({
 		scopes:['/'],
-		origins: *
+		origins: "*"
 	});
   
   
